139ee868523cc04172c7d90075cb0b134c19359b
mohit jha <mohitjha1511>
2020-09-14 12:27:20 -0700

    Updates

---
+++ precache-manifest.48e697083d934ee5a25dc0b8f8de855d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d70d9235d059e6d285fd520f25f0bc60",
+    "url": "/fetch-pokemon-Api-in-react/index.html"
+  },
+  {
+    "revision": "bb3ee6afc36a17d7da44",
+    "url": "/fetch-pokemon-Api-in-react/static/css/main.983649a8.chunk.css"
+  },
+  {
+    "revision": "8bebd7b96f44d63b5525",
+    "url": "/fetch-pokemon-Api-in-react/static/js/2.6b08d07e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fetch-pokemon-Api-in-react/static/js/2.6b08d07e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb3ee6afc36a17d7da44",
+    "url": "/fetch-pokemon-Api-in-react/static/js/main.8f7ce15b.chunk.js"
+  },
+  {
+    "revision": "500c0cfd8530ac032f7a",
+    "url": "/fetch-pokemon-Api-in-react/static/js/runtime-main.9ba5d381.js"
+  },
+  {
+    "revision": "be3f36434944658d34c48447928aa959",
+    "url": "/fetch-pokemon-Api-in-react/static/media/690074.be3f3643.jpg"
+  }
+]);
